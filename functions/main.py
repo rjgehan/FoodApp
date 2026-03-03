f259fb7b0a592520dd04325a9a512fcd7ca1204f
rjgehan <70718521+rjgehan@users.noreply.github.com>
2026-03-03 18:51:29 -0500

    initial commit

---
+++ functions/main.py
@@ -0,0 +1,9 @@
+from firebase_functions import https_fn
+from firebase_admin import initialize_app
+
+# initialize Firebase Admin
+initialize_app()
+
+@https_fn.on_request()
+def hello(req: https_fn.Request) -> https_fn.Response:
+    return https_fn.Response("Hello from Firebase API")
